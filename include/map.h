291bd46a05438baf95293788b45d51177e7d00db
Max Briggs <max@maxbriggs.org>
2021-08-02 14:06:31 -0700

    Add tile char and tile member.

--- include/map.h
+++ include/map.h
@@ -1,13 +1,16 @@
+#define MAPSIZE 32
+
 struct MAP_TILE {
 	unsigned int height  : 2; /* height is 0-3 */
 	unsigned int flooded : 1;
 	unsigned int deep    : 1; /* only if flooded */
-	unsigned int edge    : 1; /* mark edge of map */
+	unsigned int blocked    : 1; /* mark edge of map */
+	char mlet;
 };
 
 struct MAP {
 	unsigned int x, y, z;      /* map is chunk of larger world */
-	struct MAP_TILE *tiles[256][256]; /* map has 256x256 tiles */
+	struct MAP_TILE *tiles[MAPSIZE][MAPSIZE];
 };
 
 struct MAP *init_map_blank();
--- src/map.c
+++ src/map.c
@@ -1,6 +1,10 @@
 #include <stdlib.h>
 #include "map.h"
 
+#ifndef MAPSIZE
+#define MAPSIZE 256
+#endif
+
 struct MAP *init_map_blank()
 {
 	struct MAP *new_map;
@@ -11,41 +15,46 @@ struct MAP *init_map_blank()
 	new_map->y = 0;
 	new_map->z = 0;
 
-	for (int i = 0; i < 256; i++) {
+	for (int i = 0; i < MAPSIZE; i++) {
 		new_map->tiles[0][i] = (struct MAP_TILE *) malloc(sizeof(struct MAP_TILE));
 		new_map->tiles[0][i]->height = 0;
 		new_map->tiles[0][i]->flooded = 0;
 		new_map->tiles[0][i]->deep = 0;
-		new_map->tiles[0][i]->edge = 1;
+		new_map->tiles[0][i]->blocked = 1;
+		new_map->tiles[0][i]->mlet = '#';
 
-		new_map->tiles[255][i] = (struct MAP_TILE *) malloc(sizeof(struct MAP_TILE));
-		new_map->tiles[255][i]->height = 0;
-		new_map->tiles[255][i]->flooded = 0;
-		new_map->tiles[255][i]->deep = 0;
-		new_map->tiles[255][i]->edge = 1;
+		new_map->tiles[MAPSIZE - 1][i] = (struct MAP_TILE *) malloc(sizeof(struct MAP_TILE));
+		new_map->tiles[MAPSIZE - 1][i]->height = 0;
+		new_map->tiles[MAPSIZE - 1][i]->flooded = 0;
+		new_map->tiles[MAPSIZE - 1][i]->deep = 0;
+		new_map->tiles[MAPSIZE - 1][i]->blocked = 1;
+		new_map->tiles[MAPSIZE - 1][i]->mlet = '#';
 	}
 
-	for (int i = 1; i < 255; i++) {
+	for (int i = 1; i < MAPSIZE - 1; i++) {
 		new_map->tiles[i][0] = (struct MAP_TILE *) malloc(sizeof(struct MAP_TILE));
 		new_map->tiles[i][0]->height = 0;
 		new_map->tiles[i][0]->flooded = 0;
 		new_map->tiles[i][0]->deep = 0;
-		new_map->tiles[i][0]->edge = 1;
+		new_map->tiles[i][0]->blocked = 1;
+		new_map->tiles[i][0]->mlet = '#';
 
-		new_map->tiles[i][255] = (struct MAP_TILE *) malloc(sizeof(struct MAP_TILE));
-		new_map->tiles[i][255]->height = 0;
-		new_map->tiles[i][255]->flooded = 0;
-		new_map->tiles[i][255]->deep = 0;
-		new_map->tiles[i][255]->edge = 1;
+		new_map->tiles[i][MAPSIZE - 1] = (struct MAP_TILE *) malloc(sizeof(struct MAP_TILE));
+		new_map->tiles[i][MAPSIZE - 1]->height = 0;
+		new_map->tiles[i][MAPSIZE - 1]->flooded = 0;
+		new_map->tiles[i][MAPSIZE - 1]->deep = 0;
+		new_map->tiles[i][MAPSIZE - 1]->blocked = 1;
+		new_map->tiles[i][MAPSIZE - 1]->mlet = '#';
 	}
 
-	for (int i = 1; i < 255; i++) {
-		for (int j = 1; j < 255; j++) {
+	for (int i = 1; i < MAPSIZE - 1; i++) {
+		for (int j = 1; j < MAPSIZE - 1; j++) {
 			new_map->tiles[i][j] = (struct MAP_TILE *) malloc(sizeof(struct MAP_TILE));
 		new_map->tiles[i][j]->height = 0;
 		new_map->tiles[i][j]->flooded = 0;
 		new_map->tiles[i][j]->deep = 0;
-		new_map->tiles[i][j]->edge = 0;
+		new_map->tiles[i][j]->blocked = 0;
+		new_map->tiles[i][j]->mlet = ' ';
 		}
 	}
 
